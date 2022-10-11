e9c5c7efa20b551d0ce6d2e7dc7c24f5e719bacf
Pavol Galčík <galcik3@stud.uniza.sk>
2022-10-11 18:18:31 +0200

    header a footer done login formular done

---
+++ resources/views/layouts/_menu.blade.php
@@ -0,0 +1,49 @@
+<div class="row bg-dark top_menu p-0 m-0">
+    <div class="col-sm-7 logo_padding animated bounceInDown delay-1s">
+        <a href="{{ @route('welcome') }}">
+            <h1 class="logo_text"><span class="g-menu main_color">G</span><span style="color: white;">hyde</span><span class="main_color">out</span>
+            </h1>
+        </a>
+    </div>
+    <div class="col-2 user animated bounceInRight">
+        <a class="main_color log_in" href="{{ @route('login') }}">
+            <span class="icon-user"></span>
+            Log in
+        </a>
+    </div>
+
+    <div class="col-3">
+
+    </div>
+</div>
+
+<!-- menu -->
+
+    <div class="container-fluid p-0">
+        <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-center menu" id="navbarNav">
+                <ul class="navbar-nav ">
+                    <li class="nav-item hvr-overline-from-center">
+                        <a class="nav-link" href="">domov</a>
+                    </li>
+
+                    <li class="nav-item hvr-overline-from-center">
+                        <a class="nav-link" href="">novinky</a>
+                    </li>
+
+                    <li class="nav-item hvr-overline-from-center">
+                        <a class="nav-link" href="">recenzie</a>
+                    </li>
+
+                    <li class="nav-item hvr-overline-from-center">
+                        <a class="nav-link" href="">forum</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+    </div>
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+    <link rel="stylesheet" href="{{ @asset('fonts/icomoon/style.css') }}">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ @asset('css/hover-min.css') }}">
+    <link rel="stylesheet" href="{{ @asset('css/animate.css') }}">
+
+    <link rel="stylesheet" href="{{ @asset('css/style.css') }}">
+
+    <title>@yield('title', 'Ghydeout')</title>
+</head>
+<body>
+<div class="container-fluid p-0 m-0 wrapper">
+
+
+    @include('layouts._menu')
+
+    @yield('content')
+</div>
+<div class="push p-0 m-0"></div>
+    <footer class="w-100 p-2 footer">Pavol Galčík || 2022-2023</footer>
+</body>
+    <script src="//cdn.ckeditor.com/4.13.1/basic/ckeditor.js"></script> <!-- textovy editor-->
+
+    <script>
+        CKEDITOR.replace( 'editor1' );
+    </script>
+
+    <script src="{{ @asset('js/wow.min.js') }}"></script>
+
+    <script>
+        new WOW().init();
+    </script>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</html>
