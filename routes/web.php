5e42fa92d50f99dce979ba8235e5ea4d3a341a6b
Pavol Galčík <galcik3@stud.uniza.sk>
2022-12-27 16:10:06 +0100

    roles, user a admin done routes iba pre admina zabezpecene

--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,30 @@ use App\Http\Controllers\ReviewsController;
 |
 */
 
+Route::middleware(['auth', 'is_admin'])->group(function () {
+    Route::get('/admin/reviews/create', [AdminReviewsController::class, 'create'])->name('admin.reviews.create');
+
+    Route::controller(AdminReviewsController::class)->name('admin.reviews.')->group(function () {
+        Route::get('/admin/reviews', 'index')->name('index');
+        Route::get('/admin/reviews/{id}', 'show')->name('show');
+        Route::get('/admin/reviews/edit/{id}', 'edit')->name('edit');
+        Route::put('/admin/reviews/update/{id}', 'update')->name('update');
+        Route::delete('/admin/reviews/delete/{id}', 'destroy')->name('destroy');
+        //Route::get('/admin/reviews/create', 'create')->name('create');
+        Route::put('/admin/reviews/store', 'store')->name('store');
+    });
+});
+
+//middleware(['auth']);
 //Route::get('/login', [LoginController::class, 'index'])->name('login');
 
 Route::get('/reviews', [ReviewsController::class, 'index'])->name('reviews');
 
 Route::get('/reviews/{id}', [ReviewsController::class, 'show'])->whereNumber('id')->name('reviews.show');
-Route::get('/admin/reviews/create', [AdminReviewsController::class, 'create'])->name('admin.reviews.create');
+
 //Route::resource('/admin/reviews', AdminReviewsController::class)->names('admin.reviews');
 
-Route::controller(AdminReviewsController::class)->name('admin.reviews.')->group(function () {
-    Route::get('/admin/reviews', 'index')->name('index');
-    Route::get('/admin/reviews/{id}', 'show')->name('show');
-    Route::get('/admin/reviews/edit/{id}', 'edit')->name('edit');
-    Route::put('/admin/reviews/update/{id}', 'update')->name('update');
-    Route::delete('/admin/reviews/delete/{id}', 'destroy')->name('destroy');
-    //Route::get('/admin/reviews/create', 'create')->name('create');
-    Route::put('/admin/reviews/store', 'store')->name('store');
-});
+
 
 //Route::get('/admin/reviews',[AdminReviewsController::class, 'index'])->name('admin.reviews.index');
 
